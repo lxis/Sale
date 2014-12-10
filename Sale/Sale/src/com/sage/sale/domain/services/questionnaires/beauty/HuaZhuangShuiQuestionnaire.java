12b30dfa4830ec38db84d957aa48be70b5efdab0
lxis <lxis@163.com>
2014-12-10 11:20:44 +0800

    + my sale link

--- Sale/Sale/src/com/sage/sale/domain/services/questionnaires/beauty/HuaZhuangShuiQuestionnaire.java
+++ Sale/Sale/src/com/sage/sale/domain/services/questionnaires/beauty/HuaZhuangShuiQuestionnaire.java
@@ -16,19 +16,19 @@ public class HuaZhuangShuiQuestionnaire   implements IQuestionnaire {
 	}
 
 	public Product getResult() {
-		Product prox = new Product("http://cosme.pclady.com.cn/product/95015.html", 
+		Product prox = new Product("http://redirect.simba.taobao.com/rd?w=unionnojs&f=http%3A%2F%2Fai.taobao.com%2Fauction%2Fedetail.htm%3Fe%3DI1%252F92KrXEAYjmraEDZVrLjg6uUqAQ%252Bjx3G1s8WRbfpWLltG5xFicOdXrTUTgh9sMDPIwxrc30rgx5xFFx04TdSznQSvJ8jguXAYRM2Y%252BniwwzXBeaT5xzlRmtaud%252B0v%252Bi3aFqZPZa9dVo3WvHOZm5A%253D%253D%26ptype%3D100010%26from%3Dbasic&k=5ccfdb950740ca16&c=un&b=alimm_0&p=mm_106624709_8422695_28408068", 
 				"Pro-X White 纯白方程式更新角质保湿高机能水",
 				"http://img.pconline.com.cn/images/upload/upc/tx/lady_cosme/1311/11/zz/95015/95015-5_500x500.jpg");
-		Product skii = new Product("http://cosme.pclady.com.cn/product/1682.html", 
+		Product skii = new Product("http://redirect.simba.taobao.com/rd?w=unionnojs&f=http%3A%2F%2Fai.taobao.com%2Fauction%2Fedetail.htm%3Fe%3DPXS9eOWs7GbghojqVNxKsSzg%252BuK0rubhbh4mOrg73nmLltG5xFicOdXrTUTgh9sMDPIwxrc30rgx5xFFx04TdSznQSvJ8jguXAYRM2Y%252BniwwzXBeaT5xzlRmtaud%252B0v%252Bi3aFqZPZa9f%252B7C3QHBbSdQ%253D%253D%26ptype%3D100010%26from%3Dbasic&k=5ccfdb950740ca16&c=un&b=alimm_0&p=mm_106624709_8422695_28408068", 
 				"SK-II 神仙水（SK-II 护肤精华露）",
 				"http://img.pconline.com.cn/images/upload/upc/tx/lady_cosme/1311/11/zz/1682/1682-1_500x500.jpg");
-		Product yayang = new Product("http://cosme.pclady.com.cn/product/95.html",
+		Product yayang = new Product("http://redirect.simba.taobao.com/rd?w=unionnojs&f=http%3A%2F%2Fai.taobao.com%2Fauction%2Fedetail.htm%3Fe%3Dh6%252FV6b1ryT8jmraEDZVrLksZBX%252BwYAfQ3G1s8WRbfpWLltG5xFicOdXrTUTgh9sMDPIwxrc30rgx5xFFx04TdSznQSvJ8jguXAYRM2Y%252BniwwzXBeaT5xzlRmtaud%252B0v%252B0B6QeGMsXe9QkzqjaUrswA%253D%253D%26ptype%3D100010%26from%3Dbasic&k=5ccfdb950740ca16&c=un&b=alimm_0&p=mm_106624709_8422695_28408068",
 				"雅漾 舒护活泉喷雾", 
 				"http://img.pconline.com.cn/images/upload/upc/tx/lady_cosme/1402/12/c0/31189803_1392187832230_400x400.jpg");
-		Product kose = new Product("http://cosme.pclady.com.cn/product/2263.html",
+		Product kose = new Product("http://redirect.simba.taobao.com/rd?w=unionnojs&f=http%3A%2F%2Fai.taobao.com%2Fauction%2Fedetail.htm%3Fe%3DWTPkbqcAxs3ghojqVNxKsV8SyO3xc7xDN6QO%252Bdd6jj6LltG5xFicOdXrTUTgh9sMDPIwxrc30rgx5xFFx04TdSznQSvJ8jguXAYRM2Y%252BniwwzXBeaT5xzlRmtaud%252B0v%252Bi3aFqZPZa9cYMvJ8itdRfg%253D%253D%26ptype%3D100010%26from%3Dbasic&k=5ccfdb950740ca16&c=un&b=alimm_0&p=mm_106624709_8422695_28408068",
 				"高丝 雪肌精化妆水", 
 				"http://img.pconline.com.cn/images/upload/upc/tx/lady_cosme/1311/11/zz/2263/2263-4_500x500.jpg");
-		Product yashi = new Product("http://cosme.pclady.com.cn/product/48630.html",
+		Product yashi = new Product("http://redirect.simba.taobao.com/rd?w=unionnojs&f=http%3A%2F%2Fai.taobao.com%2Fauction%2Fedetail.htm%3Fe%3DTLPE36YwC0m6k0Or%252B%252BH4tCoGd%252BDKX1LLbh4mOrg73nmLltG5xFicOdXrTUTgh9sMDPIwxrc30rgx5xFFx04TdSznQSvJ8jguXAYRM2Y%252BniwwzXBeaT5xzlRmtaud%252B0v%252B0B6QeGMsXe8nPM1nRk%252FQtw%253D%253D%26ptype%3D100010%26from%3Dbasic&k=5ccfdb950740ca16&c=un&b=alimm_0&p=mm_106624709_8422695_28408068",
 				"雅诗兰黛 全新鲜亮焕采精粹水（清爽型）", 
 				"http://img.pconline.com.cn/images/upload/upc/tx/lady_cosme/1305/03/c0/20509818_1367568227596_400x400.jpg");
 
